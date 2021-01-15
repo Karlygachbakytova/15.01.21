381f68606986f33a8c6e226f6275b2faa80b030a
Your Name <you@example.com>
2021-01-15 15:20:45 +0600

    added new

--- scripts.js
+++ scripts.js
@@ -4,29 +4,36 @@ let third = second;
 let fourth = third;
 let fifth = fourth;
 console.log(fifth)
-let capital = "Washington"
-let country = "USA"
-capital = "Paris"
-country = "France"
-console.log( capital + " is the capital of " + country)
-  let subject = "distance"
-  let unit = " meters"
+let capital = "Washington";
+let country = "USA";
+capital = "Paris";
+country = "France";
+console.log( capital + " is the capital of " + country);
+  let subject = "distance";
+  let unit = " meters";
  
-  subject = "temperature"
-  unit = " Celsius "
+  subject = "temperature";
+  unit = " Celsius ";
 
-  subject = "Weight"
-  unit = " liters "   
+  subject = "Weight";
+  unit = " liters "  ; 
 
-  console.log( subject + " is measured in " + unit)
+  console.log( subject + " is measured in " + unit);
 
-  let title = "Dark Tower"
-  let author = "Stephen King"
+  let title = "Dark Tower";
+  let author = "Stephen King";
 
-  title = "Mother's field"
-  author = "Chyngyz Aimatov"
+  title = "Mother's field";
+  author = "Chyngyz Aimatov";
 
-  console.log("My favourite book is " + title + " by " + author + "!")
+  console.log("My favourite book is " + title + " by " + author + "!");
 
 
-  console.log("My favourite song is \"Yesterday\" by Beatles")
+  console.log('My favourite song is "Yesterday" by Beatles');
+
+  console.log ( 'I\'m the best');
+
+  let artist = "Beatles"
+  console.log ("I'm the fan of \" " + artist +  "\"!");
+
+  
