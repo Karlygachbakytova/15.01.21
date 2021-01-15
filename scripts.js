807ce7f288cdcee1daa90eb95cb415094c8b8fc3
Your Name <you@example.com>
2021-01-15 14:55:45 +0600

    added files

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let first = "Shrek";
+let second = first;
+let third = second;
+let fourth = third;
+let fifth = fourth;
+console.log(fifth)
+let capital = "Washington"
+let country = "USA"
+capital = "Paris"
+country = "France"
+console.log( capital + " is the capital of " + country)
+  let subject = "distance"
+  let unit = " meters"
+ 
+  subject = "temperature"
+  unit = " Celsius "
+
+  subject = "Weight"
+  unit = " liters "   
+
+  console.log( subject + " is measured in " + unit)
+
+  let title = "Dark Tower"
+  let author = "Stephen King"
+
+  title = "Mother's field"
+  author = "Chyngyz Aimatov"
+
+  console.log("My favourite book is " + title + " by " + author + "!")
